18551c225e62002713f0cf3831feb8aecb66d7b9
LuanaMonteiro0 <103607835+LuanaMonteiro0@users.noreply.github.com>
2022-10-17 11:06:55 -0300

    Add files via upload

---
+++ TRABALHO1crt.h
@@ -0,0 +1,27 @@
+#ifndef _TRABALHO1_H
+#define _TRABALHO1_H
+
+#define TAMNOME 50
+#define TAMSOBRENOME 50
+#define TAMTOT 100
+#define TAMCURSO 4
+#define MAXAL 1000
+
+
+typedef struct data{
+	int dia;
+	int mes;
+	int ano;
+}data;
+
+typedef struct registro{
+	char nome[TAMNOME];
+	char sobrenome[TAMSOBRENOME];
+	data datadenascimento;
+	int prontuario;
+	char curso[4];
+}aluno;
+
+int mainRemove(aluno *alunos, int numAlunos, int item);
+
+#endif
